dd40ed79b023ef187c1c9dbf5d6a68c363d923ce
Joji Mekkatt <jojimtt@gmail.com>
2019-01-14 16:53:23 -0500

    Updated contract script

--- ubuntu-bionic/data/curl_contract.sh
+++ ubuntu-bionic/data/curl_contract.sh
@@ -2,5 +2,5 @@
 
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"tenant":"kube","name":"all-all"}' \
+  --data '{"tenant":"kube","name":"all-all","allow-list":[{}]}' \
   http://localhost:14443/gbp/contracts
