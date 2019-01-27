6b3542ad872b876512c2a631c42a745e59aa4303
Madhu Challa <challa@noironetworks.com>
2019-01-27 10:43:55 -0800

    Untaint master if num_nodes < 3

--- ubuntu-bionic/provision_master.sh
+++ ubuntu-bionic/provision_master.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 POD_NETWORK_CIDR=$1
+NUM_NODES=$2
 
 IP=`ip -4 addr show dev enp0s8 | grep inet | awk '{print $2}' | cut -f1 -d/`
 HOST_NAME=$(hostname -s)
 
 echo "Executing inside node $HOST_NAME"
+echo "Provisioning $NUM_NODES nodes"
 
 echo "Starting kubeadm init"
 kubeadm init --apiserver-advertise-address=$IP --apiserver-cert-extra-sans=$IP --node-name $HOST_NAME --pod-network-cidr=$POD_NETWORK_CIDR
@@ -33,5 +35,9 @@ kubectl get daemonset kube-proxy -n kube-system  -o yaml > /tmp/kp.yaml
 kubectl delete -f /tmp/kp.yaml
 sed -i '/.*hostname-override.*/a\ \ \ \ \ \ \ \ - --nodeport-addresses=1.100.201.0/24' /tmp/kp.yaml
 kubectl apply -f /tmp/kp.yaml
+
+if [ $NUM_NODES -lt 3 ]; then
+    kubectl taint nodes vk8s-master node-role.kubernetes.io/master:NoSchedule-
+fi
 
 echo "Done provisioning $HOST_NAME"
