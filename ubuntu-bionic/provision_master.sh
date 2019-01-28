0035887b3cde5b6fb1dde319ee61ec75e2a2b6e4
Madhu Challa <challa@gmail.com>
2019-01-27 23:27:41 -0800

    Add packet tracing tools.

--- ubuntu-bionic/provision_master.sh
+++ ubuntu-bionic/provision_master.sh
@@ -30,10 +30,10 @@ kubectl config view > /kubeconfig/kube.yaml
 echo "Configuring aci cni"
 kubectl apply -f /home/vagrant/data/aci_deployment.yaml
 
-echo "Changing kube-proxy to use 1.100.201.0/24 subnet for NODE_PORT masquerade"
+echo "Changing kube-proxy to use $IP for NODE_PORT masquerade"
 kubectl get daemonset kube-proxy -n kube-system  -o yaml > /tmp/kp.yaml
 kubectl delete -f /tmp/kp.yaml
-sed -i '/.*hostname-override.*/a\ \ \ \ \ \ \ \ - --nodeport-addresses=1.100.201.0/24' /tmp/kp.yaml
+sed -i "/.*hostname-override.*/a\ \ \ \ \ \ \ \ - --nodeport-addresses=${IP}" /tmp/kp.yaml
 kubectl apply -f /tmp/kp.yaml
 
 if [ $NUM_NODES -lt 3 ]; then
