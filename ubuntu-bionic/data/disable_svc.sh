d81b61f73a8723ce77a91b9296c48935c234ca1a
Madhu Challa <challa@gmail.com>
2019-02-05 08:35:33 -0800

    Add scripts to disable or enable external services.

---
+++ ubuntu-bionic/data/disable_svc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MASTER_IP=1.100.201.11
+NUM_NODES=$1
+
+[ -z "$NUM_NODES" ] && NUM_NODES=2
+
+nextip ()
+{
+    ip=$1
+    iphex=$(printf '%.2X%.2X%.2X%.2X\n' `echo $ip | sed -e 's/\./ /g'`)
+    next_iphex=$(printf %.8X `echo $(( 0x$iphex + 1 ))`)
+    next_ip=$(printf '%d.%d.%d.%d\n' `echo $next_iphex | sed -r 's/(..)/0x\1 /g'`)
+    echo $next_ip
+}
+
+curr_ip=$MASTER_IP
+for i in $(seq 1 $NUM_NODES); do
+    sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@$curr_ip sudo ifconfig enp0s3 up
+    echo "External services are now disabled on $curr_ip"
+    curr_ip=$(nextip $curr_ip)
+done
---
+++ ubuntu-bionic/data/enable_svc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+FIRST_NODE_IP=1.100.201.12
+NUM_NODES=$1
+
+[ -z "$NUM_NODES" ] && NUM_NODES=2
+
+nextip ()
+{
+    ip=$1
+    iphex=$(printf '%.2X%.2X%.2X%.2X\n' `echo $ip | sed -e 's/\./ /g'`)
+    next_iphex=$(printf %.8X `echo $(( 0x$iphex + 1 ))`)
+    next_ip=$(printf '%d.%d.%d.%d\n' `echo $next_iphex | sed -r 's/(..)/0x\1 /g'`)
+    echo $next_ip
+}
+
+curr_ip=$FIRST_NODE_IP
+for i in $(seq 2 $NUM_NODES); do
+    sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@$curr_ip sudo ifconfig enp0s3 down
+    echo "External services are now enabled on $curr_ip"
+    curr_ip=$(nextip $curr_ip)
+done
+
+echo "External services are now enabled on 1.100.201.11"
+sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@1.100.201.11 sudo ifconfig enp0s3 down
